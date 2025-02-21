035871065cd072d82ad23e4301c2b5a8e01663e8
prabhatram <prabhat.ram@oulu.fi>
2025-02-21 15:14:53 +0800

    exercise #3 practice

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LuckinCoffee luckcoffee = new LuckinCoffee();
+        luckcoffee.takeOrder("1234", "2 lattes", 20.00);
+        
+    }
+}
